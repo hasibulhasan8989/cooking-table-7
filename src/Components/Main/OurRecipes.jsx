ad60ff0ad36c08366945da0fa21ab465dd4513f3
Hasibul Hasan <hasanchaion847@gmail.com>
2025-04-28 22:02:21 +0600

    Our Recipes Text

--- src/Components/Main/OurRecipes.jsx
+++ src/Components/Main/OurRecipes.jsx
@@ -4,7 +4,7 @@ const OurRecipes = () => {
         <div className=" text-center space-y-4 mt-24 mb-12">
             
         <h1 className="font-bold text-3xl">Our Recipes</h1>
-        <p className="max-w-[823px] mx-auto">Lorem ipsum dolor sit amet consectetur. Proin et feugiat senectus vulputate netus pharetra rhoncus. Eget urna volutpat curabitur elementum mauris aenean neque. </p>
+        <p className="max-w-[823px] mx-auto">Savor every bite at our restaurant — where fresh flavors, warm hospitality, and unforgettable dishes come together for a dining experience you’ll love! </p>
 
 
         </div>
